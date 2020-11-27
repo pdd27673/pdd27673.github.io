0928bce98a04c1d18f1f7ce98da86c2ec11de74d
Paul Doho <paul.doho.741@my.csun.edu>
2020-11-27 15:56:19 -0800

    Personnalize me feature

--- app.js
+++ app.js
@@ -30,4 +30,84 @@ window.onload = function () {
 					}
 				);
 		});
+
+	console.log('Its working');
+
+	let theme = localStorage.getItem('theme');
+	let themeStyle = document.getElementById('theme-style');
+	let themeDots = document.getElementsByClassName('theme-dot');
+
+	if (theme == null) {
+		setTheme('light');
+	} else {
+		setTheme(theme);
+	}
+
+	for (var i = 0; themeDots.length > i; i++) {
+		themeDots[i].addEventListener('click', function () {
+			let mode = this.dataset.mode;
+			console.log('Option clicked:', mode);
+			setTheme(mode);
+		});
+	}
+
+	function setTheme(mode) {
+		if (mode == 'light') {
+			themeStyle.href = 'styles.css';
+		}
+
+		if (mode == 'blue') {
+			document.getElementById('theme-style').href = 'blue.css';
+		}
+
+		if (mode == 'green') {
+			document.getElementById('theme-style').href = 'green.css';
+		}
+
+		if (mode == 'purple') {
+			document.getElementById('theme-style').href = 'purple.css';
+		}
+
+		localStorage.setItem('theme', mode);
+	}
 };
+
+// console.log('Its working');
+
+// let theme = localStorage.getItem('theme');
+// let themeStyle = document.getElementById('theme-style');
+// let themeDots = document.getElementsByClassName('theme-dot');
+
+// if (theme == null) {
+// 	setTheme('light');
+// } else {
+// 	setTheme(theme);
+// }
+
+// for (var i = 0; themeDots.length > i; i++) {
+// 	themeDots[i].addEventListener('click', function () {
+// 		let mode = this.dataset.mode;
+// 		console.log('Option clicked:', mode);
+// 		setTheme(mode);
+// 	});
+// }
+
+// function setTheme(mode) {
+// 	if (mode == 'light') {
+// 		themeStyle.href = 'styles.css';
+// 	}
+
+// 	if (mode == 'blue') {
+// 		document.getElementById('theme-style').href = 'blue.css';
+// 	}
+
+// 	if (mode == 'green') {
+// 		document.getElementById('theme-style').href = 'green.css';
+// 	}
+
+// 	if (mode == 'purple') {
+// 		document.getElementById('theme-style').href = 'purple.css';
+// 	}
+
+// 	localStorage.setItem('theme', mode);
+// }
